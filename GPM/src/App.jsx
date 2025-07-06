5198d541f93c8b8715d10d0faf9b70327629f990
ParasNn <40841618+ParasNn@users.noreply.github.com>
2025-07-06 17:04:55 -0500

    added project view to main branch

--- GPM/src/App.jsx
+++ GPM/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './Pages/Home';
+import ProjectView from './Pages/ProjectView';
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <Layout>
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/projectview" element={<ProjectView />} />
                 </Routes>
             </Layout>
         </Router>
