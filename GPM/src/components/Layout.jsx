b0cdeebf20b4d301eeebc0abfded182e89666bbc
Aamir811 <77645339+Aamir811@users.noreply.github.com>
2025-07-15 16:46:37 -0500

    Readding everything to the main page

--- GPM/src/components/Layout.jsx
+++ GPM/src/components/Layout.jsx
@@ -1,12 +1,13 @@
+import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import './Layout.css';
 
-function Layout({ children }) {
+function Layout() {
     return (
         <div className="layout">
             <Sidebar />
             <main className="main-content">
-                {children}
+                <Outlet />
             </main>
         </div>
     );
